8483c490cb69e9f82780d8e2fd069d3c3bf9e178
Fretux <frederik@spirgi.com>
2026-01-26 11:34:48 +0100

    Refactor command and event loading

---
+++ loaders/events.js
@@ -0,0 +1,23 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const isJavaScriptFile = (file) => file.endsWith('.js');
+
+const getEventFilePaths = (eventsPath) =>
+    fs
+        .readdirSync(eventsPath)
+        .filter(isJavaScriptFile)
+        .map((file) => path.join(eventsPath, file));
+
+const loadEventModules = (eventsPath) => {
+    const eventFiles = getEventFilePaths(eventsPath);
+
+    return eventFiles.map((filePath) => ({
+        filePath,
+        event: require(filePath),
+    }));
+};
+
+module.exports = {
+    loadEventModules,
+};
